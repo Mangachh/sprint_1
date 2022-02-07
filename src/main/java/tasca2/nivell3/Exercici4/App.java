3a867c5016bf2cf422d946767747f90baf155eb1
Mangachh <cutre.studio.base@gmail.com>
2022-02-07 15:31:38 +0100

    add comments

--- src/main/java/tasca2/nivell3/Exercici4/App.java
+++ src/main/java/tasca2/nivell3/Exercici4/App.java
@@ -1,7 +1,9 @@
 package tasca2.nivell3.Exercici4;
 
 public class App {
-    //TODO: SIN HACER!
+    // No hace falta hacerlo.
+
+    
     /* Exercici 4. Afegeix un mètode finalitza() a la classe 
      * FailingConstrictor i escriu el codi necessari 
      * per a utilitzar adequadament aquesta classe.
